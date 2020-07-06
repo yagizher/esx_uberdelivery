da5d229253bf634d7c5db088bc1913b5c67b01e6
Yagiz ALP <yagizwasmeow@gmail.com>
2020-07-06 13:49:51 +0300

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,10 +1,6 @@
 ESX = nil
-local lastStarted = 0
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-	TriggerEvent('esx_addonaccount:getSharedAccount', 'society_grove', function(account)
-		societyAccountGrove = account
-	end)
 
 RegisterServerEvent('esx_uberkdshfksksdhfskdjjob:pay')
 AddEventHandler('esx_uberkdshfksksdhfskdjjob:pay', function(amount)
@@ -12,10 +8,3 @@ AddEventHandler('esx_uberkdshfksksdhfskdjjob:pay', function(amount)
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.addMoney(tonumber(amount))
 end)
---Settings--
---[[ Optional SocietyAccount ( You have to implement it )
-local societyAccount = nil
- TriggerEvent('esx_addonaccount:getSharedAccount', 'society_mecano', function(account)
-    societyAccount = account
- end)
-]]
